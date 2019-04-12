db39412e4c0353af65dc5f9a3f3b2b256f123d35
Mark Ellis <mark@cs.utexas.edu>
2019-04-12 14:09:18 -0500

    ; sql updates

--- app/src/main/java/com/example/bowlstats/addPlayer.java
+++ app/src/main/java/com/example/bowlstats/addPlayer.java
@@ -1,20 +1,35 @@
 package com.example.bowlstats;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.view.View;
 import android.widget.Button;
+import java.util.*;
 
 public class addPlayer extends AppCompatActivity {
 
+    sqlHelper sh = new sqlHelper(this);
+    SQLiteDatabase db= sh.getWritableDatabase();
     private Button menu;
+    private TextView txt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_player);
         menu = findViewById(R.id.menu);
+        txt = findViewById(R.id.names);
+
+        sh.addPlayer("Mark");
+        sh.addPlayer("Daniel");
+        sh.addPlayer("Thu");
+
+        List<String> players = new LinkedList<String>();
+        players = sh.displayPlayers();
+
 
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -23,6 +38,8 @@ public class addPlayer extends AppCompatActivity {
                 startActivity(myIntent);
             }
         });
+
+
     }
 
 
--- app/src/main/java/com/example/bowlstats/sqlHelper.java
+++ app/src/main/java/com/example/bowlstats/sqlHelper.java
@@ -1,24 +1,30 @@
 package com.example.bowlstats;
 
+import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
+import java.util.*;
+import android.database.Cursor;
 
 public class sqlHelper extends SQLiteOpenHelper {
 
     private static final String TAG = "DatabaseHelper";
 
-    private static final String TABLE_NAME = "stats_sheet";
-    private static final String COL1 = "Date";
-    private static final String COL2 = "Game1";
-    private static final String COL3 = "Game2";
-    private static final String COL4 = "Game3";
-    private static final String COL5 = "Day Games";
-    private static final String COL6 = "Total Games";
-    private static final String COL7 = "Total Average";
-    private static final String COL8 = "High Score";
-    private static final String COL9 = "Low Score";
-    private static final String COL10 = "Total points";
+    static final String TABLE_NAME = "Names";
+    static final String COL1 = "Name";
+
+//    private static final String TABLE_NAME = "stats_sheet";
+//    private static final String COL1 = "Date";
+//    private static final String COL2 = "Game1";
+//    private static final String COL3 = "Game2";
+//    private static final String COL4 = "Game3";
+//    private static final String COL5 = "Day Games";
+//    private static final String COL6 = "Total Games";
+//    private static final String COL7 = "Total Average";
+//    private static final String COL8 = "High Score";
+//    private static final String COL9 = "Low Score";
+//    private static final String COL10 = "Total points";
 
 
 
@@ -28,15 +34,46 @@ public class sqlHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE " + TABLE_NAME + " (" + COL1 + "DATE," + COL2 + "SMALLINT," +
-                COL3 + "SMALLINT," + COL4 + "SMALLINT," + COL5 + "SMALLINT," + COL6 + "FLOAT," + COL7 +
-                "SMALLINT," + COL8 + "SMALLINT," + COL9 + "SMALLINT," + COL10 + "SMALLINT)";
+        String createTable = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + " (" + COL1 + "TEXT)";
+//        String deleteTable = "DROP TABLE IF EXISTS " + TABLE_NAME;
         db.execSQL(createTable);
     }
+
+//    @Override
+//    public void onCreate(SQLiteDatabase db) {
+//        String createTable = "CREATE TABLE " + TABLE_NAME + " (" + COL1 + "DATE," + COL2 + "SMALLINT," +
+//                COL3 + "SMALLINT," + COL4 + "SMALLINT," + COL5 + "SMALLINT," + COL6 + "FLOAT," + COL7 +
+//                "SMALLINT," + COL8 + "SMALLINT," + COL9 + "SMALLINT," + COL10 + "SMALLINT)";
+//        db.execSQL(createTable);
+//    }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+
+    public void addPlayer(String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COL1, name);
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public List<String> displayPlayers() {
+        List<String> players = new LinkedList<String>;
+
+        String query = "SELECT * FROM " + TABLE_NAME;
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+        String player = null;
+        if(cursor.moveToFirst()) {
+            do {
+                player = cursor.getString(0);
+                players.add(player);
+            } while(cursor.moveToNext());
+        }
+        return players;
     }
 
 
