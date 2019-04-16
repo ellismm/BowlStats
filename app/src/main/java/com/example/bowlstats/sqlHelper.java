8f93d95eb21f6ab7baf87152d753b89cadbf962d
Mark Ellis <mark@cs.utexas.edu>
2019-04-16 13:51:16 -0500

    working on updating the stats for a player

--- app/src/main/java/com/example/bowlstats/sqlHelper.java
+++ app/src/main/java/com/example/bowlstats/sqlHelper.java
@@ -1,6 +1,10 @@
 package com.example.bowlstats;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.database.CharArrayBuffer;
+import android.database.ContentObserver;
+import android.database.DataSetObserver;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.content.Context;
@@ -11,6 +15,8 @@ import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
 import android.os.strictmode.SqliteObjectLeakedViolation;
 import android.util.Log;
 
@@ -32,6 +38,7 @@ public class sqlHelper extends SQLiteOpenHelper {
     private static final String totalAverage = "Total_Average";
     private static final String highScore = "High_Score";
     private static final String lowScore = "Low_Score";
+    private static final String dayPoints = "Day_Points";
     private static final String totalPoints = "Total_Points";
 
 
@@ -79,7 +86,7 @@ public class sqlHelper extends SQLiteOpenHelper {
                     Game1 + " SMALLINT(300), " + Game2 + " SMALLINT(300), " + Game3 + " SMALLINT(300), " +
                     gamesToday + " SMALLINT(3), " + totalGames + " SMALLINT, " + dayAverage + " FLOAT(300.3), " +
                     totalAverage + " FLOAT, " + highScore + " SMALLINT(300), " + lowScore + " SMALLINT(300), " +
-                    totalPoints + " BIGINT " + ") ";
+                    dayPoints + " SMALLINT(900), " + totalPoints +  " BIGINT " + ") ";
             db.execSQL(createTable);
             return true;
         }
@@ -132,15 +139,85 @@ public class sqlHelper extends SQLiteOpenHelper {
         return player;
     }
 
+    // Record new scores and updates stats
     public void recordScore(String name, int score) {
+
+        // Name of the specified persons database;
+        NAME_DB = name;
+
+        // Initializing temp variables to use throughout
+        int tempInt, tempInt2, tempInt3;
+        String tempDate;
+        Float tempFloat, tempFloat2;
+
+        // Initializing cursors
+        Cursor cursor1, cursor2;
+
         SQLiteDatabase db = getWritableDatabase();
+//        SQLiteDatabase db2 = getReadableDatabase();
         ContentValues values = new ContentValues();
 
-        DateFormat formatDate = new SimpleDateFormat("yyyy-dd-mm");
+        DateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date();
-        //formatDate.format(date);
-        int tempDate = date.getDate();
-        System.out.println(formatDate.format(date));
+        tempDate = formatDate.format(date);
+        System.out.println(tempDate);
+
+        cursor2 = db.rawQuery("SELECT * FROM " + NAME_DB + " WHERE " + theDate + " = " +
+                tempDate, null);
+        // If this is the first game of the day
+        if(cursor2.getCount() == 0) {
+            values.put(theDate, tempDate);
+            values.put(Game1, score);
+            values.put(Game2, -1);
+            values.put(Game3, -1);
+            values.put(gamesToday, 1);
+            values.put(dayAverage,(float) score);
+            values.put(dayPoints, score);
+//            tempInt2 = cursor1.getInt(cursor1.getColumnIndex(dayPoints));
+//            tempFloat = cursor1.getFloat(cursor1.getColumnIndex(dayAverage));
+
+            // select all the current current rows to see if there is any games played yet
+            cursor1 = db.rawQuery(" SELECT * FROM " + NAME_DB, null);
+
+            // If this is the first game for this player
+            if (cursor1.getCount() == 0) {
+                values.put(totalGames, 1);
+                values.put(totalPoints, score);
+                values.put(dayPoints, score);
+                values.put(lowScore, score);
+                values.put(highScore, score);
+            }
+
+            // If this is not the first game for this player
+            else {
+                // get totalGames to add to it.
+                cursor1.moveToLast();
+                tempInt = cursor1.getInt(cursor1.getColumnIndex(totalGames));
+                tempInt++;
+                values.put(totalGames, tempInt);
+
+                // Update the totalPoints for this player
+                tempInt3 = cursor1.getInt(cursor1.getColumnIndex(totalPoints));
+                tempInt3 += score;
+
+                // Update the totalPoints and totalAverage for this player
+                values.put(totalPoints, tempInt3);
+                values.put(totalAverage, tempInt3 / tempInt);
+
+                // get the low score and the high score for this player
+                tempInt = cursor1.getInt(cursor1.getColumnIndex(lowScore));
+                tempInt2 = cursor1.getInt(cursor1.getColumnIndex(highScore));
+
+                // Update the low and high score for this player
+                values.put(lowScore, tempInt < score ? tempInt : score);
+                values.put(highScore, tempInt2 > score ? tempInt2 : score);
+            }
+        }
+
+        // If this is the 2nd or 3rd game
+        else if (cursor2.getCount() > 0) {
+
+        }
 //        values.put(theDate, )
     }
 
