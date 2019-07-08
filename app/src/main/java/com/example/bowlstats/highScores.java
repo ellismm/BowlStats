5fdd813dc464c5807195ab78900fb15c63b16d01
ellismm <mark.m.ellis@utexas.edu>
2019-07-07 20:09:19 -0500

    added circle buttons and a tracker for splits and weeks 2 get a 300

--- app/src/main/java/com/example/bowlstats/highScores.java
+++ app/src/main/java/com/example/bowlstats/highScores.java
@@ -1,26 +1,33 @@
 package com.example.bowlstats;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class highScores extends AppCompatActivity {
     private Button home, highScore, lowScore, highAverage, mostPoints, mostGames, won,
-            lastFour, gameOne, gameTwo, gameThree;
+            lastFour, gameOne, gameTwo, gameThree, weeks_plus, weeks_minus, split_minus, split_plus;
     private TextView message;
+    private EditText splits, weeks;
     String name;
     int score;
     float score2;
     sqlHelper sh = new sqlHelper(this);
+
+    public static final String week_saved = "Weeks_Saved";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_high_scores2);
+
+        SharedPreferences wee = getSharedPreferences(week_saved, MODE_PRIVATE);
 
         home = findViewById(R.id.home);
         highScore = findViewById(R.id.highScore);
@@ -33,10 +40,24 @@ public class highScores extends AppCompatActivity {
         gameOne = findViewById(R.id.gameOne);
         gameTwo = findViewById(R.id.gameTwo);
         gameThree = findViewById(R.id.gameThree);
+        split_plus = findViewById(R.id.split_plus);
+        split_minus = findViewById(R.id.split_minus);
+        splits = findViewById(R.id.split);
+        weeks_plus = findViewById((R.id.weeks_plus));
+        weeks_minus = findViewById(R.id.weeks_minus);
+        weeks = findViewById(R.id.weeks_left);
+
+
 
         message = findViewById(R.id.message);
         sh.winCalculations();
 
+
+        int wees = wee.getInt("wees", 0);
+        int splis = wee.getInt("splis", 0);
+
+        weeks.setText(String.valueOf(wees));
+        splits.setText(String.valueOf(splis));
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -144,6 +165,62 @@ public class highScores extends AppCompatActivity {
                 name = sh.averageScoreGameThree();
                 score2 = sh.getAverageScoreGameThree(name);
                 message.setText("\n\n" + name + " has a Game 3 average of: \n" + score2);
+            }
+        });
+
+        // decrement the number of weeks left
+        weeks_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences wee2 = getApplicationContext().getSharedPreferences(week_saved, MODE_PRIVATE);
+                SharedPreferences.Editor editor = wee2.edit();
+                int x  = wee2.getInt("wees", 0);
+                x--;
+                editor.putInt("wees", x);
+                editor.commit();
+                weeks.setText(String.valueOf(x));
+            }
+        });
+
+        // Increment the number of weeks left
+        weeks_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences wee2 = getApplicationContext().getSharedPreferences(week_saved, MODE_PRIVATE);
+                SharedPreferences.Editor editor = wee2.edit();
+                int x  = wee2.getInt("wees", 0);
+                x++;
+                editor.putInt("wees", x);
+                editor.commit();
+                weeks.setText(String.valueOf(x));
+            }
+        });
+
+        // decrement the number of splits
+        split_minus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences wee2 = getApplicationContext().getSharedPreferences(week_saved, MODE_PRIVATE);
+                SharedPreferences.Editor editor = wee2.edit();
+                int x = wee2.getInt("splis", 0);
+                x--;
+                editor.putInt("splis", x);
+                editor.commit();
+                splits.setText(String.valueOf(x));
+            }
+        });
+
+        // Increment the number of splits
+        split_plus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences wee2 = getApplicationContext().getSharedPreferences(week_saved, MODE_PRIVATE);
+                SharedPreferences.Editor editor = wee2.edit();
+                int x  = wee2.getInt("splis", 0);
+                x++;
+                editor.putInt("splis", x);
+                editor.commit();
+                splits.setText(String.valueOf(x));
             }
         });
     }
